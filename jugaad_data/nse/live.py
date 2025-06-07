b44f1d135902a9c035a7f802bb828569ef0aa9a7
vijaykumardas <vijaykumardas@gmail.com>
2025-06-07 19:04:51 +0530

    Added jugaad-data

---
+++ jugaad_data/nse/live.py
@@ -0,0 +1,149 @@
+"""
+    Implements live data fetch functionality
+"""
+from datetime import datetime
+from requests import Session
+from ..util import live_cache
+class NSELive:
+    time_out = 5
+    base_url = "https://www.nseindia.com/api"
+    page_url = "https://www.nseindia.com/get-quotes/equity?symbol=LT"
+    _routes = {
+            "stock_meta": "/equity-meta-info",
+            "stock_quote": "/quote-equity",
+            "stock_derivative_quote": "/quote-derivative",
+            "market_status": "/marketStatus",
+            "chart_data": "/chart-databyindex",
+            "market_turnover": "/market-turnover",
+            "equity_derivative_turnover": "/equity-stock",
+            "all_indices": "/allIndices",
+            "live_index": "/equity-stockIndices",
+            "index_option_chain": "/option-chain-indices",
+            "equity_option_chain": "/option-chain-equities",
+            "currency_option_chain": "/option-chain-currency",
+            "pre_open_market": "/market-data-pre-open",
+            "holiday_list": "/holiday-master?type=trading",
+            "corporate_announcements": "/corporate-announcements"
+    }
+    
+    def __init__(self):
+        self.s = Session()
+        h = {
+            "Host": "www.nseindia.com",
+            "Referer": "https://www.nseindia.com/get-quotes/equity?symbol=SBIN",
+            "X-Requested-With": "XMLHttpRequest",
+            "pragma": "no-cache",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            }
+        self.s.headers.update(h)
+        self.s.get(self.page_url)
+
+    def get(self, route, payload={}):
+        url = self.base_url + self._routes[route]
+        r = self.s.get(url, params=payload)
+        return r.json()
+
+    @live_cache
+    def stock_quote(self, symbol):
+        data = {"symbol": symbol}
+        return self.get("stock_quote", data) 
+
+    @live_cache
+    def stock_quote_fno(self, symbol):
+        data = {"symbol": symbol}
+        return self.get("stock_derivative_quote", data)
+
+    @live_cache
+    def trade_info(self, symbol):
+        data = {"symbol": symbol, "section": "trade_info"}
+        return self.get("stock_quote", data) 
+
+    @live_cache
+    def market_status(self):
+        return self.get("market_status", {})
+
+    @live_cache
+    def chart_data(self, symbol, indices=False):
+        data = {"index" : symbol + "EQN"}
+        if indices:
+            data["index"] = symbol
+            data["indices"] = "true"
+        return self.get("chart_data", data)
+    
+    @live_cache
+    def tick_data(self, symbol, indices=False):
+        return self.chart_data(symbol, indices)
+
+    @live_cache
+    def market_turnover(self):
+        return self.get("market_turnover")
+
+    @live_cache
+    def eq_derivative_turnover(self, type="allcontracts"):
+        data = {"index": type}
+        return self.get("equity_derivative_turnover", data)
+    
+    @live_cache
+    def all_indices(self):
+        return self.get("all_indices")
+
+    def live_index(self, symbol="NIFTY 50"):
+        data = {"index" : symbol}
+        return self.get("live_index", data)
+    
+    @live_cache
+    def index_option_chain(self, symbol="NIFTY"):
+        data = {"symbol": symbol}
+        return self.get("index_option_chain", data)
+
+    @live_cache
+    def equities_option_chain(self, symbol):
+        data = {"symbol": symbol}
+        return self.get("equity_option_chain", data)
+
+    @live_cache
+    def currency_option_chain(self, symbol="USDINR"):
+        data = {"symbol": symbol}
+        return self.get("currency_option_chain", data)
+
+    @live_cache
+    def live_fno(self):
+        return self.live_index("SECURITIES IN F&O")
+    
+    @live_cache
+    def pre_open_market(self, key="NIFTY"):
+        data = {"key": key}
+        return self.get("pre_open_market", data)
+    
+    @live_cache
+    def holiday_list(self):
+        return self.get("holiday_list", {})
+
+    def corporate_announcements(self, segment='equities', from_date=None, to_date=None, symbol=None):
+        """
+            This function returns the corporate annoucements 
+            (https://www.nseindia.com/companies-listing/corporate-filings-announcements)
+        """
+
+        #from_date: 02-12-2024
+        #to_date: 06-12-2024
+        #symbol: 
+        payload = {"index": segment}
+
+        if from_date and to_date:
+            payload['from_date'] = from_date.strftime("%d-%m-%Y")
+            payload['to_date']   = to_date.strftime("%d-%m-%Y")
+        elif from_date or to_date:
+            raise Exception("Please provide both from_date and to_date")
+        if symbol:
+            payload['symbol'] = symbol
+        return self.get("corporate_announcements", payload)
+
